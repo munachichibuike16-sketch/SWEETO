380ed90a03de05676d0f6df563ab94345cd5cd73
munachichibuike16-sketch <munachichibuike16@gmail.com>
2026-06-10 16:13:03 +0000

    chore: Remove debug coordinate alert

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -78,14 +78,6 @@ const CheckoutPage = () => {
       (position) => {
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
-        const acc = Math.round(position.coords.accuracy);
-        
-        // Temporary debug alert
-        alert(lang === 'fr' 
-          ? `GPS: ${lat}, ${lng} (Précision: ${acc}m)` 
-          : `GPS: ${lat}, ${lng} (Accuracy: ${acc}m)`
-        );
-
         setCustomCoords({ lat, lng });
         setGpsSuccess(true);
         setGpsLoading(false);
