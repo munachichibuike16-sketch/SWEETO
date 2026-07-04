448e205a0670cabf1f41e1e1203941b2ce459fb3
Sweeto Dev <sweeto@example.com>
2026-07-04 12:28:37 +0000

    feat: use high-resolution transparent PNG version of the custom bird logo

--- src/components/SweetoLogo.jsx
+++ src/components/SweetoLogo.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SweetoLogo = ({ size = 120, className = '', animate = true }) => {
   return (
     <img 
-      src="/sweeto_logo.jpg"
+      src="/sweeto_logo.png"
       alt="Sweeto Hub"
       style={{ width: size }}
-      className={`select-none cursor-pointer object-contain rounded-lg ${className}`}
+      className={`select-none cursor-pointer object-contain ${className}`}
     />
   );
 };
